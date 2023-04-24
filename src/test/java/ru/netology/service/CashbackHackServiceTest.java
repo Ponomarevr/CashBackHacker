d82ba12a0fedd8d6073ccedc06c8faf71a1dfbbf
Roman Ponomarev <ponomarevr1986@gmail.com>
2023-04-24 14:59:09 +0300

    Tests added at testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,55 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemainIfLowerThan1000() {
+        int amount = 800;
+        int expected = 200;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void testRemainIf1000() {
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+    @Test
+    public void testRemainIf0() {
+        int amount = 0;
+        int expected = 1000;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+    @Test
+    public void testRemainIfMoreThan1000() {
+        int amount = 1800;
+        int expected = 200;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+    @Test
+    public void testRemainIf3000() {
+        int amount = 3000;
+        int expected = 0;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+
+    }
+}
